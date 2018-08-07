54e122dac28df877da766b1a60443b553b9b1497
Caio Sanches Bentes <caiosbentes@users.noreply.github.com>
2018-08-07 09:15:09 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -13,3 +13,13 @@ service vboxdrv start
 usermod -a -G vboxusers caiobentes
 sudo dnf install -y https://linux.dropbox.com/packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm
 dnf install nmap
+#Instalação do Telegram Desktop
+wget "https://telegram.org/dl/desktop/linux" -O telegram.tar.xz
+tar Jxf telegram.tar.xz -C /opt/
+mv /opt/Telegram*/ /opt/telegram
+ln -sf /opt/telegram/Telegram /usr/bin/telegram
+echo -e '[Desktop Entry]\n Version=1.0\n Exec=/opt/telegram/Telegram\n Icon=Telegram\n Type=Application\n Categories=Application;Network;' | sudo tee /usr/share/applications/telegram.desktop
+chmod +x /usr/share/applications/telegram.desktop
+cp /usr/share/applications/telegram.desktop  /home/caiobentes/Área\ de\ trabalho/
+sudo dnf install snapd
+sudo ln -s /var/lib/snapd/snap /snap
