e98cfe6575b88db5ac9563c09cd795ffba2caa64
Caio Sanches Bentes <caiosbentes@users.noreply.github.com>
2018-10-16 08:59:22 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -29,3 +29,8 @@ dnf install terminator
 dnf install transmission
 dnf install freedrp
 dnf install hplib-gui
+dnf install openssh-server
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+sudo dnf install sublime-text
+sudo dnf install -y remmina*
